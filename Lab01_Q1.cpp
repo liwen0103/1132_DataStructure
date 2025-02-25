8c06475d3c6e5d559f6c6d50f7768cc83a339c2a
liwen0103 <t113310241@ntut.org.tw>
2025-02-25 20:38:30 +0800

    Update Lab01_Q1.cpp

--- Lab01_Q1.cpp
+++ Lab01_Q1.cpp
@@ -1,1 +1,21 @@
-
+class Solution 
+{
+public:
+    bool isMonotonic(vector<int>& nums) 
+    {
+        bool increasing = true, decreasing = true;//兩者狀態皆可能是monotonic array
+        
+        for (int i = 1; i < nums.size(); i++) 
+        {
+            if (nums[i] > nums[i - 1]) //兩數為遞增狀態
+            {
+                decreasing = false;//遞增狀態下如果遞減則不是monotonic array
+            }
+             if (nums[i] < nums[i - 1])//兩數為遞減狀態
+             {
+                increasing = false;//遞減狀態下如果遞增則也不是monotonic array
+             }
+        }
+        return increasing || decreasing;
+    }
+};
